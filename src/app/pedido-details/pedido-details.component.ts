006e6ebd9d7d04943a597984ba573f4555748721
Klezya <benjamin.nunez.beat@gmail.com>
2024-11-13 22:32:31 -0300

    cree un componente

---
+++ src/app/pedido-details/pedido-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-details',
+  templateUrl: './pedido-details.component.html',
+  styleUrls: ['./pedido-details.component.scss'],
+})
+export class PedidoDetailsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
